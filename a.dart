9e16fff9f56e03b96c528577a8ecf64c684a89b1
faheemjat3 <94822656+faheemjat3@users.noreply.github.com>
2021-12-11 02:55:53 -0800

    Add files via upload

---
+++ a.dart
@@ -0,0 +1,16 @@
+
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter Your Name : ");
+  var name = stdin.readLineSync();
+  stdout.write("Enter Your Age : ");
+  var age = stdin.readLineSync();
+  var agee = int.parse(age ?? "");
+
+  int? v=
+
+  print(" ");
+}
+
+
